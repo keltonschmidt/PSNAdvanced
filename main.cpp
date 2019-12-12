d29d96ce8bbd6f42f2ae1e01a6702b474bea3a30
keltonschmidt <53916785+keltonschmidt@users.noreply.github.com>
2019-12-12 07:26:06 +0100

    Second Commit

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <sys/types.h>
 #include <string.h>
 #include "windows.h"
+#include "codesLIB.h"
 
 #define PatternNum 18
 #define PatternChars 12
@@ -64,6 +65,7 @@ void readPatternArray();
 int revealPattern();
 int checkPattern();
 int generateFromCol(int colNumber);
+int generateFromNum(int colNumber, int rowNumber);
 string currentdate();
 
 
@@ -93,7 +95,6 @@ int main() {
 	string path = "Results//" + date + "//AMAZON CODES.txt";
     codici.open(path.c_str(),  ios::out);
     /*****************************************************************************/
-    
 	
     /*   TRASFORMO IL CONTENUTO DELLA MATRICE PATTERN E LA STAMPO A VIDEO INSIEME AL SET DI PATTERN GENERATI   */
     readPatternArray();
@@ -167,20 +168,18 @@ string currentdate()
 }
 
 void readPatternArray() {
-	int i = 0, j = 0, k = 0;
+	int i = 0, j = 0, k = 0, l = 0;
 	
 	cout << "Importing Pattern's Database..." << '\n' << endl;
-	//cout << "Studying Pattern's Database..." << endl;
-	cout << '\t' << "Current Database: " << '\t' << '\t' << '\t' << "Generated Database: " << '\n' << endl;
+	cout << '\t' << "Current Database: " << '\t' << '\t' << "Generated Database: " << '\n' << endl;
 	
 	
 	for(i = 0; i < 18; i++) {
 		cout << '\t';
-		//for(j = 0; j < 12; j++) {
-		//	cout << patterns[i][j];
-		//}
 		
 		cout << "    |    ";
+		
+		cout << l << ")" << '\t';
 		
 		for(k = 0; k < 12; k++) {
 			if(k == 4 || k == 8) {
@@ -195,6 +194,10 @@ void readPatternArray() {
 		
 		cout << "    |    ";
 		
+		
+		cout << l << ")" << '\t';
+		//l++;
+		
 		for(j = 0; j < 12; j++) {
 			if((j == 4) || (j == 8)) {
 				cout << "-";
@@ -202,15 +205,16 @@ void readPatternArray() {
 			
 			if(rand() % 2 == 0) {
 				secondPatternGroup[i][j] = 0;
-				cout << "X";
+				cout << "0";
 			} else {
 				secondPatternGroup[i][j] = 1;
-				cout << "0";
+				cout << "X";
 			}
 			
 			//cout << secondPatternGroup[i][j];			
 		}
 		
+		l++;
 		cout << endl;
 	}
 }
@@ -248,8 +252,8 @@ void generateRand() {
  ********************************/
 
 void showMenu() {
-	int select = 0, colNum = 0, rowNum = 0;
-	cout << "    " << "----------------------------------------------------------------" << endl;
+	int select = 0, colNum = 0, rowNum = 0, patternNum = 0;
+	cout << "         " << "----------------------------------------------------------------" << endl;
 	cout << "1) Generate a new Set of Patterns." << endl;
 	cout << "2) Select a Column." << endl;
 	cout << "3) Select a Pattern." << endl;
@@ -276,6 +280,8 @@ void showMenu() {
 			cin >> colNum;
 			cout << "Insert row number: ";
 			cin >> rowNum;
+			
+			generateFromNum(colNum, rowNum);
 			
 			break;
 	}
@@ -345,16 +351,16 @@ int generateFromCol(int colNum) {
 	int j = 0, tempN = 0;
 	char tempC;
 	int amount = 0;
+	int cont = 0;
 	
 	cout << "Insert amount of codes: ";
 	cin >> amount;
 	
 	if(colNum == 0) {
-		while(j < amount) {
-			cout << "Current Pattern: " << endl;
+		do {
+			cont++;
 			for(i = 0; i < 12; i++) {
-				currentPattern[i] = patterns[j][i];
-				cout << currentPattern[i];
+				currentPattern[i] = patterns[i][j];
 			}
 		
 		    for(i = 0; i < 12; i++) {
@@ -371,10 +377,87 @@ int generateFromCol(int colNum) {
 		    }
 		    cout << endl;
 		    
-		    j++;
-		    if(j == 18) {
-		    	j = 0;
+		    if(cont >= 18) {
+		    	cont = 0;
 			}
-		}	
+			j++;
+			
+		} while(j < amount);
+			
+	} else {
+		do {
+			cont++;
+			for(i = 0; i < 12; i++) {
+				currentPattern[i] = secondPatternGroup[i][j];
+			}
+		
+		    for(i = 0; i < 12; i++) {
+		        if(i == 4 || i == 8) {
+		            cout << "-";
+		        }
+		        if(currentPattern[i] == 0) {
+		            tempN = rand() % 10;
+		            cout << tempN;
+		        } else {
+		            tempC = (rand() % 26) + 65;
+		            cout << tempC;
+		        }
+		    }
+		    cout << endl;
+		    
+		    if(cont >= 18) {
+		    	cont = 0;
+			}
+			j++;
+			
+		} while(j < amount);	
 	}
+}
+
+int generateFromNum(int colNumber, int rowNumber) {
+	int rowCont = 0, i = 0, j = 0, k = 0, amount = 0;
+	
+	cout << "Insert amount of codes: ";
+	cin >> amount;
+	cout << endl;
+	
+	if(colNumber == 0) {
+		for(k = 0; k < 12; k++) {
+			if(k == 4 || k == 8) {
+           		cout << "-";
+	        }
+	        if(patterns[rowNumber][k] == 0) {
+	            cout << "0";
+	            currentPattern[k] = 0;
+	        } else {
+	            cout << "X";
+	            currentPattern[k] = 1;
+	        }
+		}
+		
+		i++;
+	} else {
+		for(k = 0; k < 12; k++) {
+			if(k == 4 || k == 8) {
+           		cout << "-";
+	        }
+	        if(secondPatternGroup[rowNumber][k] == 0) {
+	            cout << "0";
+	            currentPattern[k] = 0;
+	        } else {
+	            cout << "X";
+	            currentPattern[k] = 1;
+	        }
+		}
+		
+		i++;
+	}
+	
+	while(j < amount) {
+		revealPattern();
+		
+		j++;
+	}
+	
+	return 1;
 }
